af0c30491a5a4f5744c36b102f11c1bfa4dbe5f9
Sergey Gonin <sergon146@gmail.com>
2018-05-12 09:36:10 +0500

    init stetho, leakcanary

--- app/src/main/java/com/sergon146/mobilization18/App.java
+++ app/src/main/java/com/sergon146/mobilization18/App.java
@@ -5,9 +5,11 @@ import android.app.Application;
 import android.content.res.Resources;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.stetho.Stetho;
 import com.sergon146.core.Core;
 import com.sergon146.mobilization18.di.base.AppInjector;
 import com.sergon146.mobilization18.di.component.AppComponent;
+import com.squareup.leakcanary.LeakCanary;
 
 import javax.inject.Inject;
 
@@ -41,9 +43,18 @@ public class App extends Application implements HasActivityInjector {
     @Override
     public void onCreate() {
         super.onCreate();
+        initTools();
         instance = this;
         AppInjector.init(this);
         initCore();
+    }
+
+    private void initTools() {
+        if (!LeakCanary.isInAnalyzerProcess(this)) {
+            LeakCanary.install(this);
+        }
+
+        Stetho.initializeWithDefaults(this);
         Fresco.initialize(this);
     }
 
